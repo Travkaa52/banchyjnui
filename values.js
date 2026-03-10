0cf0d91abb039831db7a04933baa1c7798e7db90
Travkaa52 <dimakasan028@gmail.com>
2026-03-10 13:23:39 +0200

    Add files via upload

---
+++ values.js
@@ -0,0 +1,77 @@
+// values.js
+
+// Основные данные 
+var fio               = "Чумаченко Максим Олегович";
+var fio_en            = "08.09.2006";
+var birth             = "08.09.2006"; //дата рождения
+var date_give         = "11.10.2024"; //Дата видачи
+var date_out          = "03.11.2035"; // действителен до
+var organ             = "0512"; //орган что выдал документ
+var rnokpp            = "3582200550"; //ИНН
+var uznr              = "19980128-07937"; //Номер записи
+var pass_number       = "008420840"; //номер паспорта
+
+var registeredOn      = "20.09.1999"; //дата регистрации
+
+// Прописка
+var legalAdress       = "Харківська область"; //Место проживание
+var live              = "Харківська область"; //Место рождение 
+var bank_adress       = "Харківська область, Харківський район м.Харків вул.Гарібальді,буд. 2, кв. 5"; //Место жительства указано в банке
+
+var sex               = "Ж";
+var sex_en            = "W";
+
+// Данные для Прав
+var rights_categories = "A, B"; //Категории
+var prava_number      = "AUX723027"; // номер прав
+var prava_date_give   = "01.04.2022"; //Дата выдачи Прав
+var prava_date_out    = "01.04.2032"; //Действителен ДО
+var pravaOrgan        = "0512"; //орган который выдал
+
+
+
+var university        = "ХНУ имени Каразина"; // Університет
+var fakultet          = "Физико-технический"; // Факультет
+var stepen_dip        = "Магістра";
+var univer_dip        = "ХНУ имени Каразина";
+var dayout_dip        = "01.07.2023";
+var special_dip       = "Прикладная математика";
+var number_dip        = "MT-545678";
+var form              = "Очная";
+
+
+// заграник
+var zagran_number     = "FX862524"; //номер загран
+var dateGiveZ         = "18.11.2019"; //выдан загран
+var dateOutZ          = "18.11.2029"; //коньчаеться загран
+
+var student_number    = "2022154258";
+var student_date_give = "01.09.2021";
+var student_date_out  = "30.06.2025";
+
+
+
+// Включение/выключение документов
+var isRightsEnabled   = true;
+var isZagranEnabled   = true;
+var isDiplomaEnabled  = true;
+var isStudyEnabled    = true;
+// Добавь остальные переменные по аналогии
+
+// Пути к нужным фото    ?timestamp=N   "photo.jpg?timestamp=N" динамически: var url = "photo.jpg?timestamp=" + N;
+
+var photo_passport = "1.png"; //эДок, паспорт,
+var photo_rights   = "1.png"; //права
+var photo_students = "1.png"; //диплом, студ
+var photo_zagran   = "1.png"; //загран
+
+var signPng           = "sign.png"; //подпись
+// Можно добавить еще, если надо
+
+
+
+
+
+
+
+
